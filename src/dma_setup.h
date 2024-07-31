7ec831e9b0384bd5fdc0f1c92cabc4220c380260
Anton Evmenenko <anton.evmenenko.93@gmail.com>
2024-08-01 01:38:54 +0200

    File .clang-format updated and all the files in src/ updated based on the new rules

--- src/dma_setup.h
+++ src/dma_setup.h
@@ -5,16 +5,16 @@
 #include "config.h"
 
 void MX_DMA_Init(void) {
-  /* DMA controller clock enable */
-  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_DMAMUX1);
-  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_DMA1);
+    /* DMA controller clock enable */
+    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_DMAMUX1);
+    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_DMA1);
 
-  /* DMA interrupt init */
-  /* DMA1_Channel1_IRQn interrupt configuration */
-  // NVIC_SetPriority(
-  //     DMA1_Channel1_IRQn,
-  //     NVIC_EncodePriority(NVIC_GetPriorityGrouping(), DMA_ADC_PRIORITY, 0));
-  // NVIC_EnableIRQ(DMA1_Channel1_IRQn);
+    /* DMA interrupt init */
+    /* DMA1_Channel1_IRQn interrupt configuration */
+    // NVIC_SetPriority(
+    //     DMA1_Channel1_IRQn,
+    //     NVIC_EncodePriority(NVIC_GetPriorityGrouping(), DMA_ADC_PRIORITY, 0));
+    // NVIC_EnableIRQ(DMA1_Channel1_IRQn);
 }
 
 #endif  // DMA_SETUP_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
-#include "blocks_lib.h"
 #include "adc_setup.h"
+#include "blocks_lib.h"
 #include "dma_setup.h"
 #include "gpio_setup.h"
 #include "nvic.h"
@@ -26,101 +26,97 @@ int16_t beta = 0;
 
 SelectorMotorType motor_sel(motor_type, sup_vltg, alpha, beta, INT16_MIN);
 SelectorInterconnectPwm4ch pwm_mux(pattern_pwm, motor_sel.getPwmChannels());
-ModuleDriverPWM pwm(pwm_mode,
-                    pwm_resolution,
-                    sup_vltg,
-                    pwm_mux.getPwmChannels());
+ModuleDriverPWM pwm(pwm_mode, pwm_resolution, sup_vltg, pwm_mux.getPwmChannels());
 
 uint16_t current_sensor_A, current_sensor_B, voltage_vref, temperature;
 
 extern "C" void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim) {
-  if (htim->Instance == TIM2) {
-    static bool underflow = true;
-    if (underflow = !underflow) {
-      // TODO use TIM2_Set_PWM_Values here
-      TIM2_Set_PWM_Values(pwm.getPwmChannels());
+    if (htim->Instance == TIM2) {
+        static bool underflow = true;
+        if (underflow = !underflow) {
+            // TODO use TIM2_Set_PWM_Values here
+            TIM2_Set_PWM_Values(pwm.getPwmChannels());
 
-      // NVIC_SetPendingIRQ(EXTI0_IRQn);
-      // NVIC_SetPendingIRQ(EXTI1_IRQn);
-      current_sensor_A = dma_buffer[0];
-      current_sensor_B = dma_buffer[1];
-      sup_vltg = dma_buffer[2] << 3;
-      voltage_vref = dma_buffer[3] << 3;
-      temperature = dma_buffer[4];
-    } else {
-      ADC1_StartDMAConversion();
+            // NVIC_SetPendingIRQ(EXTI0_IRQn);
+            // NVIC_SetPendingIRQ(EXTI1_IRQn);
+            current_sensor_A = dma_buffer[0];
+            current_sensor_B = dma_buffer[1];
+            sup_vltg = dma_buffer[2] << 3;
+            voltage_vref = dma_buffer[3] << 3;
+            temperature = dma_buffer[4];
+        } else {
+            ADC1_StartDMAConversion();
+        }
     }
-  }
 }
 
 void init_debug_pin() {
-  // EXP_TX | pc10
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  GPIO_InitTypeDef GPIO_InitStruct;
-  GPIO_InitStruct.Pin = GPIO_PIN_10;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FAST;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+    // EXP_TX | pc10
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    GPIO_InitTypeDef GPIO_InitStruct;
+    GPIO_InitStruct.Pin = GPIO_PIN_10;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FAST;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 }
 
 extern "C" void EXTI0_IRQHandler(void) {
-  NVIC_ClearPendingIRQ(EXTI0_IRQn);
+    NVIC_ClearPendingIRQ(EXTI0_IRQn);
 
-  // TODO Add high speed loop math implementation here
+    // TODO Add high speed loop math implementation here
 }
 
 extern "C" void EXTI1_IRQHandler(void) {
-  NVIC_ClearPendingIRQ(EXTI1_IRQn);
+    NVIC_ClearPendingIRQ(EXTI1_IRQn);
 
-  // TODO Add FOC implementation here
-  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_10);
-  delayMicroseconds(1);
-  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_10);
+    // TODO Add FOC implementation here
+    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_10);
+    delayMicroseconds(1);
+    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_10);
 }
 
 void setup() {
-  init_debug_pin();
-  GPIO_Init();
-  // NVIC_Init();
-  MX_DMA_Init();
-  MX_ADC1_Init((uint16_t*)dma_buffer, DMA_ADC_BUFFER_SIZE);
-  MX_ADC1_Start();
-  MX_TIM2_Init();
-  MX_TIM2_Start();
+    init_debug_pin();
+    GPIO_Init();
+    // NVIC_Init();
+    MX_DMA_Init();
+    MX_ADC1_Init((uint16_t*)dma_buffer, DMA_ADC_BUFFER_SIZE);
+    MX_ADC1_Start();
+    MX_TIM2_Init();
+    MX_TIM2_Start();
 
-
-  digitalWriteFast(PB_2, HIGH);
-  digitalWriteFast(PA_4, HIGH);
+    digitalWriteFast(PB_2, HIGH);
+    digitalWriteFast(PA_4, HIGH);
 }
 
 const float step = 0.01 * PI;  // Step of 0.05 PI
 float angle = 0.0;
 
-unsigned long previousMicros = 0;  // Store the last time the code was executed
+unsigned long previousMicros = 0;    // Store the last time the code was executed
 const unsigned long interval = 500;  // Interval in microseconds
 
 void loop() {
-  motor_sel.tick();
-  pwm_mux.tick();
-  pwm.tick();
+    motor_sel.tick();
+    pwm_mux.tick();
+    pwm.tick();
 
-  // Generate sin and cos values with amplitude 10000
-  alpha = int16_t(sin(angle) * motor_voltage);
-  beta = int16_t(cos(angle) * motor_voltage);
+    // Generate sin and cos values with amplitude 10000
+    alpha = int16_t(sin(angle) * motor_voltage);
+    beta = int16_t(cos(angle) * motor_voltage);
 
-  // Increase the angle by step
-  angle += step;
+    // Increase the angle by step
+    angle += step;
 
-  // Reset the angle if it exceeds 2*PI
-  if (angle >= 2 * PI) {
-    angle -= 2 * PI;
-  }
+    // Reset the angle if it exceeds 2*PI
+    if (angle >= 2 * PI) {
+        angle -= 2 * PI;
+    }
 
-  // SerialUSB.print(">Sup: " + String(sup_vltg));
-  // SerialUSB.print(">Temp: " + String(temperature));
-  // SerialUSB.print(">Vref: " + String(voltage_vref));
+    // SerialUSB.print(">Sup: " + String(sup_vltg));
+    // SerialUSB.print(">Temp: " + String(temperature));
+    // SerialUSB.print(">Vref: " + String(voltage_vref));
 
-  // Add a delay to control the loop execution speed
-  delayMicroseconds(3000);
+    // Add a delay to control the loop execution speed
+    delayMicroseconds(3000);
 }
