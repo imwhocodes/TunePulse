e9435362ae05319f7e681e5493b310edd76c9f3b
Luca Passarella <passarella.luca96@gmail.com>
2024-08-05 01:17:48 +0200

    intital implementation of IO for Blocks

--- lib/blocks/generic_block.h
+++ lib/blocks/generic_block.h
@@ -9,13 +9,12 @@
 #ifndef GENERIC_BLOCK_H
 #define GENERIC_BLOCK_H
 
+#include <USBDevice.h>
 #include <float.h>
 #include <inttypes.h>
 #include "control_modes.h"
 
 #include "data_types.h"
-
-
 
 // ################### GENERIC BLOCK #################################
 
@@ -29,11 +28,11 @@
  */
 
 class Block {
- public:
-  /**
-   * @brief Pure virtual function of Block callback.
-   */
-  virtual void tick() = 0;
+   public:
+    /**
+     * @brief Pure virtual function of Block callback.
+     */
+    virtual void tick() = 0;
 };
 
 // ################### MACROS #################################
@@ -45,8 +44,8 @@ class Block {
  * @param name The name of the input variable.
  */
 #define BLOCK_INPUT(type, name) \
- protected:                     \
-  const type& name##_;
+   protected:                   \
+    const type& name##_;
 
 /**
  * @brief Macro to define an output channel in a Block.
@@ -54,13 +53,63 @@ class Block {
  * @param type The data type of the output.
  * @param name The name of the output variable.
  */
-#define BLOCK_OUTPUT(type, name)   \
- protected:                        \
-  type name##_;                    \
-                                   \
- public:                           \
-  const type& get_##name() const { \
-    return name##_;                \
-  }
+#define BLOCK_OUTPUT(type, name)     \
+   protected:                        \
+    type name##_;                    \
+                                     \
+   public:                           \
+    const type& get_##name() const { \
+        return name##_;              \
+    }
+
+// ################### BLOCK IO #################################
+
+template <typename T>
+class Input;
+
+template <typename T>
+class Output {
+    T value;
+
+   public:
+    constexpr Output<T>(T&& input) : value{input} {}
+
+    constexpr operator T&() { return this->value; }
+
+    constexpr T& operator=(T&& new_value) {
+        this->value = new_value;
+        return this->value;
+    };
+
+    // constexpr T& operator[](int i) { this->value[i] }
+
+    constexpr Input<T> asInput() { return this->value; }
+
+    friend Input<T>;
+};
+
+template <typename T>
+class Input {
+    const T& value;
+
+   public:
+    constexpr Input<T>(const T& as_input) : value{as_input} {}
+    constexpr Input<T>(const Output<T>& as_input) : value{as_input.value} {}
+    constexpr operator const T&() const { return this->value; }
+    constexpr const T& asValue() const { return this->value; }
+};
+
+template <typename T>
+using Variable = Output<T>;
+
+// void test() {
+//     Output<int> v(0);
+
+//     Input<int> i(v);
+
+//     v = 5;
+
+//     // i = 0;
+// }
 
 #endif  // GENERIC_BLOCK_H
